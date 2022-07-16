3b16532eb2a2774aac1e275e75d86a10eebcac46
bBSempai <bbwhacker0805@gmail.com>
2022-07-16 21:00:21 +0530

    minor fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <div class="xl:w-152 lg:w-120 md:w-160 sm:w-160 sm:relative sm:left-0 lg:relative lg:left-0">
         <Feed />
       </div>
-      <div class="xl:w-88 xl:block lg:hidden md:hidden sm:hidden zero:hidden">
+      <div class="xl:w-88 xl:block lg:hidden md:hidden sm:hidden">
         <SideContainer />
       </div>
     </div>
